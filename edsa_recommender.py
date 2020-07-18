7a7aefbc980ab836c97894afb86fa031c2340f56
georgetshego <60339076+georgetshego@users.noreply.github.com>
2020-07-18 19:40:22 +0200

    edit

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -32,8 +32,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# Open images
-from PIL import Image
 # Custom Libraries
 from utils.data_loader import load_movie_titles
 from recommenders.collaborative_based import collab_model
@@ -47,7 +45,7 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Welcome","About the App", "Recommender System","Solution Overview", "Data Visualisation","Contact Us" ]
+    page_options = ["Recommender System","Solution Overview"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
@@ -73,12 +71,16 @@ def main():
         # Perform top-10 movie recommendation generation
         if sys == 'Content Based Filtering':
             if st.button("Recommend"):
-                with st.spinner('Crunching the numbers...'):
-                    top_recommendations = content_model(movie_list=fav_movies,
-                                                        top_n=1)
-                st.title("We think you'll like:")
-                for i,j in enumerate(top_recommendations):
-                    st.subheader(str(i+1)+'. '+j)
+                try:
+                    with st.spinner('Crunching the numbers...'):
+                        top_recommendations = content_model(movie_list=fav_movies,
+                                                            top_n=10)
+                    st.title("We think you'll like:")
+                    for i,j in enumerate(top_recommendations):
+                        st.subheader(str(i+1)+'. '+j)
+                except:
+                    st.error("Oops! Looks like this algorithm does't work.\
+                              We'll need to fix it!")
 
 
         if sys == 'Collaborative Based Filtering':
@@ -86,11 +88,10 @@ def main():
                 try:
                     with st.spinner('Crunching the numbers...'):
                         top_recommendations = collab_model(movie_list=fav_movies,
-                                                           top_n=1)
+                                                           top_n=10)
                     st.title("We think you'll like:")
                     for i,j in enumerate(top_recommendations):
-                        st.subheader(top_recommendations)
-                        #st.subheader(str(i+1)+'. '+j)
+                        st.subheader(str(i+1)+'. '+j)
                 except:
                     st.error("Oops! Looks like this algorithm does't work.\
                               We'll need to fix it!")
@@ -99,24 +100,10 @@ def main():
     # -------------------------------------------------------------------
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
-    if page_selection == "About the App":
-        st.title("About the App")
-        st.markdown("Are you a movie lover? Are you tired of wasting your time watching tons of trailers and ending up not watching their movies? Are you tired of finishing your popcorns before you find the right movie? Not anymore!!")
-        st.image(["images/tired1.jpg", "images/tired22.jpg"],width=200)
-        st.markdown("You have come to the right app. Based on your favourite watched movies, The Movie Recommender App filters or predicts your preferences based on your choices. With just just a few clicks, you will select three of your most favourite movies from thousands on the app and you will get one movie you are most likely to enjoy")
-        
     if page_selection == "Solution Overview":
         st.title("Solution Overview")
-        st.markdown("The app uses recommender systems to produce your recommendation in any of the two ways –")
-        st.markdown("**Collaborative filtering**: which builds a model from your past behavior (i.e. movies watched or selected by the you) as well as similar decisions made by other users.")
-        st.markdown("**Content-based filtering**: which uses a series of discrete characteristics of your selected movies in order to recommend additional movies with similar properties.")
-    if page_selection == "Welcome":
-        st.markdown("![Alt Text](https://github.com/Clarencia/unsupervised-predict-streamlit-template/blob/master/images/welcome.gif?raw=true)")
-        st.markdown("![Alt Text](https://cdn.clipart.email/11d8b5822102da1a7c7a2d015a569485_animated-popcorn-clipart-gif_350-350.gif)")
-    if page_selection == "Contact Us":
-        st.title("Connect with us")
-        st.markdown("For support or any questions contact us here:")
-        st.image(["images/contact.PNG"])
+        st.write("Describe your winning approach on this page")
+
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
 
