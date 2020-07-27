7001c447b72f78c2441bff34c950193ce6b80d54
georgetshego <60339076+georgetshego@users.noreply.github.com>
2020-07-27 14:08:05 +0200

    edit

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -140,7 +140,7 @@ def main():
     if page_selection ==  "Movie Insights":
         st.title("Movie Insights")
         st.markdown('This page gives you all the insights you must have about movies from the IMDb and The Movie DB sites. The visuals will be updated everytime a new movie is uploaded into on of the mentioned websites.')
-        insights= st.radio("Select a visual you would like to see",('Pie chart displaying a count ratings', 'Number of movies in each genre', 'Proportion of genres per year', 'Genre performance per year', 'Top 50 words in movie titles','Distribution of movies based on runtime', 'Change in movies runtime over the years'))
+        insights= st.radio("Select a visual you would like to see",('Pie chart displaying a count ratings', 'Number of movies in each genre', 'Proportion of genres per year', 'Genre performance per year', 'Top 50 words in movie titles','Distribution of movies based on runtime', 'Change in movies runtime over the years',"View all visuals"))
         if insights=='Pie chart displaying a count ratings':
             st.image(Image.open("images/pie_ratings_vs.PNG"))
         elif insights =='Number of movies in each genre':
